f3b48cb2d20e04c0e28d269d4593e2ce49502956
Chehab Mustafa-Hilmy <m@chehab.me>
2014-08-25 19:08:37 +0300

    [FIX] indentaction function doc, [ADDED] return plain text on reques.

--- lapt.py
+++ lapt.py
@@ -1,23 +1,28 @@
+#!/usr/bin/env python3
 # -*- charset= utf-8 -*-
 
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
 @app.route("/M/<uid>")
 def member_login(uid): #
-''':uid: members RFID'''
-    pass
+    ''':uid: members RFID'''
+    return 'member_login'
 
 @app.route("/V/<uid>")
 def visitor_login(uid):
-''':uid: visitor tooken, ie: mobile number'''
-    pass
+    ''':uid: visitor tooken, ie: mobile number'''
+    return 'visitor_login'
 
-@app.route("/", ['get', 'post'])
+@app.route("/", methods=['get', 'post'])
 def visitor_resigtration():
-''':uid: visitor tooken, ie: mobile number'''
-    pass
+    ''':uid: visitor tooken, ie: mobile number'''
+    if request.method == 'POST':
+        return 'visitor_resigtration [POST]'
+    else:
+        return 'visitor_resigtration [GET]'
+    return 'visitor_resigtration (ERROR)'
 
 # DB stuff
 
@@ -30,6 +35,7 @@ def db_set():
 
 if __name__ == '__main__':
     app.run(
+        host='0.0.0.0',
         port=3000,
         debug=True,
     )
